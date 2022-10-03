9a5e3285990efcda4f2310f5686edf04d9078e59
Tis4tom <tdmorgan_mac@icloud.com>
2022-10-03 18:00:56 -0400

    add active trail function

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('green-bg');
   menuList.classList.toggle('display-none');  
 });
+
+
+const activePage = window.location.pathname;
+const menuItems = document.querySelectorAll('.menu-list li a');
+
+for (let menuItem of menuItems) {
+  if (menuItem.href.includes(`${activePage}`)) {
+    menuItem.classList.add('active');
+  }
+}
+
